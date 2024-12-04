218a233d78b73a0d3e4549af4c76a954ec4dc754
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2024-12-04 12:55:35 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ObstacleDetectionApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ObstacleDetectionApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
